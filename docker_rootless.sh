c7ce27f10a6918e907a1ff971535e297c907c36f
HuntTheSun <boandlkramer793@gmail.com>
2025-08-02 09:58:47 +0200

    fixed docker rootless script, stop and disable docker system daemon

--- docker_rootless.sh
+++ docker_rootless.sh
@@ -8,5 +8,6 @@ echo
 sudo apt install slirp4netns dbus-user-session docker-ce-rootless-extras uidmap -y
 dockerd-rootless-setuptool.sh install
 
-sudo systemctl disable docker.service --now
+sudo systemctl disable docker.service
+sudo systemctl stop docker.service
 
